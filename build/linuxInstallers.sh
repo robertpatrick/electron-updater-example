0df5aa6e0accedd65b86f2786269d383a2b5ea79
Robert Patrick <robert.patrick@oracle.com>
2021-09-25 12:32:51 -0500

    testing linux builds

---
+++ build/linuxInstallers.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(dirname "$0")
+export WORKSPACE=$(cd "${SCRIPT_DIR}/.."; pwd)
+if [ "${WKTUI_DEV_PROXY}" != "" ]; then
+  export WKTUI_PROXY=${WKTUI_DEV_PROXY}
+fi
+
+BUILDER_VERSION=14-05.21
+WKTUI_ENV_FILE="${WORKSPACE}/docker.env"
+echo "WKTUI_USER=$(id -un)" > "${WKTUI_ENV_FILE}"
+# shellcheck disable=SC2129
+echo "WKTUI_UID=$(id -u)" >> "${WKTUI_ENV_FILE}"
+echo "WKTUI_GROUP=$(id -gn)" >> "${WKTUI_ENV_FILE}"
+echo "WKTUI_GID=$(id -g)" >> "${WKTUI_ENV_FILE}"
+
+if [ -n "${WKTUI_PROXY}" ]; then
+  echo "HTTPS_PROXY=${WKTUI_PROXY}" >> "${WKTUI_ENV_FILE}"
+fi
+
+docker run --rm -v "${WORKSPACE}:/project" --env-file "${WKTUI_ENV_FILE}" electronuserland/builder:${BUILDER_VERSION} /project/build/linuxInstallersInDocker.sh
