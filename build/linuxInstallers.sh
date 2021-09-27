8f4b373a3bac5185d70a2c10515139ecba0d624c
Robert Patrick <robert.patrick@oracle.com>
2021-09-27 08:35:27 -0500

    adding GH_TOKEN to docker environment

--- build/linuxInstallers.sh
+++ build/linuxInstallers.sh
@@ -6,6 +6,11 @@ if [ "${WKTUI_DEV_PROXY}" != "" ]; then
   export WKTUI_PROXY=${WKTUI_DEV_PROXY}
 fi
 
+if [ -z "${GH_TOKEN}" ]; then
+  echo "GH_TOKEN environment variable must be set prior to running this script">&2
+  exit 1
+fi
+
 BUILDER_VERSION=14-05.21
 WKTUI_ENV_FILE="${WORKSPACE}/docker.env"
 echo "WKTUI_USER=$(id -un)" > "${WKTUI_ENV_FILE}"
@@ -13,6 +18,7 @@ echo "WKTUI_USER=$(id -un)" > "${WKTUI_ENV_FILE}"
 echo "WKTUI_UID=$(id -u)" >> "${WKTUI_ENV_FILE}"
 echo "WKTUI_GROUP=$(id -gn)" >> "${WKTUI_ENV_FILE}"
 echo "WKTUI_GID=$(id -g)" >> "${WKTUI_ENV_FILE}"
+echo "GH_TOKEN=${GH_TOKEN}" >> "${WKTUI_ENV_FILE}"
 
 if [ -n "${WKTUI_PROXY}" ]; then
   echo "HTTPS_PROXY=${WKTUI_PROXY}" >> "${WKTUI_ENV_FILE}"
